f16f019fe968bad937865683ad14984df36f868a
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-06-17 23:10:32 +0200

    WIP: DBHelper established

---
+++ MHRBuildTool/MHRBuildTool/DBHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+
+namespace MHRBuildTool
+{
+    public class DBHelper
+    {
+        public static DataTable ExecuteDataSet(string sql, CommandType cmdType, params SqlParameter[] parameters)
+        {
+            using (DataSet ds = new DataSet())
+            using (SqlConnection connection = new SqlConnection("testconnectionString"))
+            using (SqlCommand cmd = new SqlCommand(sql, connection))
+            {
+                cmd.CommandType = cmdType;
+                foreach(SqlParameter item in parameters)
+                {
+                    cmd.Parameters.Add(item);
+                }
+                try
+                {
+                    cmd.Connection.Open();
+                    new SqlDataAdapter(cmd).Fill(ds);
+                }
+                catch(SqlException ex)
+                { };
+                return ds.Tables[0];
+            }
+        }
+    }
+}
